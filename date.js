e1722ae823fdc72c449f03861236f5a494f8cfb9
Jonas Elvedal Hole <jonas@jole.xyz>
2021-03-24 13:24:54 +0100

    local date

---
+++ date.js
@@ -0,0 +1,22 @@
+// For todays date;
+Date.prototype.today = function () { 
+    return this.getFullYear() + "-" +(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) + "-" + ((this.getDate() < 10)?"0":"") + this.getDate();
+}
+
+// For the time now
+Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
+
+function localize(){
+	console.log("localize");
+
+	var all = document.getElementsByClassName("posttime")
+
+	for (var i=0, max=all.length; i < max; i++) {
+	  var o = all[i].innerHTML.replace(/-/g, "/")
+	  var t = new Date(o);
+	  all[i].innerHTML=t.today() + " " + t.timeNow() + " <small>(" + Intl.DateTimeFormat().resolvedOptions().timeZone + "</small>)";
+	}
+}
